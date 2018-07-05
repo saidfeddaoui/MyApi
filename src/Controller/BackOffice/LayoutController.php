96b1a003a9f7256d4638efd672fce5ddd74e3690
El Maguiri Omar <elmaguiri@mobiblanc.com>
2018-07-05 08:58:14 +0100

    Integration et developpement d'espace MAMDA (Add Security)

--- src/Controller/BackOffice/LayoutController.php
+++ src/Controller/BackOffice/LayoutController.php
@@ -3,12 +3,11 @@
 namespace App\Controller\BackOffice;
 
 use App\Entity\InsuranceType;
-use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * @Route(name="backoffice_layout_")
@@ -23,13 +22,19 @@ class LayoutController extends Controller
      *
      * @param InsuranceType $insuranceType
      * @param SessionInterface $session
+     * @param AuthorizationCheckerInterface $authorizationChecker
      * @return Response
      */
-    public function changeMutuelle(InsuranceType $insuranceType, SessionInterface $session)
+    public function changeMutuelle(InsuranceType $insuranceType, SessionInterface $session, AuthorizationCheckerInterface $authorizationChecker)
     {
-        $session->set('insuranceType', serialize($insuranceType));
+        if ($authorizationChecker->isGranted($insuranceType->getRole())) {
+            $session->set('insuranceType', serialize($insuranceType));
+        }
         return $this->redirect($this->generateUrl('list_ville'));
     }
+    /**
+     * @return Response
+     */
     public function renderInsuranceTypesList()
     {
         $insurances = $this->getUser()->getInsuranceTypes();
@@ -37,6 +42,10 @@ class LayoutController extends Controller
             'insurances' => $insurances
         ]);
     }
+    /**
+     * @param SessionInterface $session
+     * @return Response
+     */
     public function renderThemeColor(SessionInterface $session)
     {
         $color = self::DEFAULT_THEME_COLOR;
