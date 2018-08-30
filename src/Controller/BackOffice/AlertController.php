f79108eb8de515f16d967eaf2f2e8dea47163427
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-08-30 13:46:14 +0100

    add image

--- src/Controller/BackOffice/AlertController.php
+++ src/Controller/BackOffice/AlertController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\BackOffice;
 
 use App\Entity\Alert;
+use App\Entity\Attachment;
 use App\Form\AlertType;
 use Ramsey\Uuid\Uuid;
 use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
