575184bfe488ec9630b23d8f2e6f7241e1ccdf02
said <feddaoui@mobiblanc.com>
2018-09-04 15:07:35 +0100

    update device

--- src/Entity/Device.php
+++ src/Entity/Device.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use JMS\Serializer\Annotation as Serializer;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
