ad2f85169c46802ce1f06f959f94e3ee83670b7f
fanane <fanane@mobiblanc.com>
2018-09-06 16:54:16 +0100

    add WS add/device

--- src/Services/ApiDevices.php
+++ src/Services/ApiDevices.php
@@ -27,59 +27,24 @@ class ApiDevices
     // Device entity
     public function setDevice($device_params,$dateTime)
     {
-        $devices = $this->em->getRepository(Device::class)->findOneBy(array('device_uid'=>$device_params['device_uid']));
-        if (!$devices instanceof Device){
-            $devices = new Device();
+        $devices = $this->em->getRepository(Devices::class)->findOneBy(array('device_uid'=>$device_params['device_uid']));
+        if (!$devices instanceof Devices){
+            $devices = new Devices();
             $devices->setDeviceUid($device_params['device_uid']);
             $devices->setOs($device_params['os']);
             $devices->setModel($device_params['model']);
             $devices->setCreatedAt($dateTime);
-            $devices->setVersionCode($device_params['version_code']);
-            $devices->setFirebaseToken($device_params['firebase_token']?:'');
-            $devices->setPushable($device_params['pushable']);
-            $devices->setLatitude($device_params['latitude']);
-            $devices->setLongitude($device_params['longitude']);
-            $devices->setUpdatedAt($dateTime);
-            $this->em->persist($devices);
-            $this->em->flush();
-        }else{
-            $devices->setVersionCode($device_params['version_code']);
-            $devices->setFirebaseToken($device_params['firebase_token']?:'');
-            $devices->setPushable($device_params['pushable']);
-            $devices->setLatitude($device_params['latitude']);
-            $devices->setLongitude($device_params['longitude']);
-            $devices->setUpdatedAt($dateTime);
-            $this->em->flush();
         }
-
-    }
-
-    public function checkVersion($os, $version_name){
-       // $os = $this->em->getRepository(OperatingSystem::class)->findOneBy(array('name' => strtolower($os)));
-        $versions = $this->em->getRepository(Version::class)->findOneBy(array('os'=>$os));
-
-        if ($versions instanceof Version){
-            if (version_compare($version_name, $versions->getMinVersion()) <= 0){
-                return array(
-                    'status' => "ERROR",
-                    'message' => $versions->getMsgBloquer()?:'',
-                    'url' => $versions->getPath()?:'',
-                    'cache' => $versions->getCache()?true:false,
-                );
-            }
-
-            if ((version_compare($version_name, $versions->getMinVersion()) > 0) && (version_compare($version_name, $versions->getCurrentVersion()) < 0)) {
-               return array(
-                    'status' => "WARNING",
-                    'message' => $versions->getMsgInfo(),
-                    'url' => $versions->getPath()?:'',
-                    'cache' => $versions->getCache()?true:false,
-               );
-            }
-        }
-
-        return array('status'=>'OK','message'=>'SUCCESSFULY UPDATED');
-
+        $devices->setVersionName($device_params['version_name']);
+        $devices->setVersionCode($device_params['version_code']);
+        $devices->setFirebaseToken($device_params['firebase_token']?:'');
+        $devices->setPushable($device_params['pushable']);
+        $devices->setLatitude($device_params['latitude']);
+        $devices->setLongitude($device_params['longtitude']);
+        $devices->setUpdatedAt($dateTime);
+        $this->em->persist($devices);
+        $this->em->flush();
+        return true;
     }
 
 
