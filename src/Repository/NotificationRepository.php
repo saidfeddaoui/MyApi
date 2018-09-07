e0a75e684c6c33e80ef0b522f7a73fbecdf4141a
fanane <fanane@mobiblanc.com>
2018-09-07 10:31:48 +0100

    push notification cmd

--- src/Repository/NotificationRepository.php
+++ src/Repository/NotificationRepository.php
@@ -7,10 +7,16 @@
  */
 
 namespace App\Repository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
-
-class NotificationRepository
+class NotificationRepository extends ServiceEntityRepository
 {
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Notification::class);
+    }
+
     public function getPushClient()
     {
         return $this->createQueryBuilder('p')
