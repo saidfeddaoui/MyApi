d53e80ba10d07fbd708fbc34fae14f217fd72074
fanane <fanane@mobiblanc.com>
2018-09-07 09:02:36 +0100

    add releation between client and device

--- src/Controller/Api/RegistrationController.php
+++ src/Controller/Api/RegistrationController.php
@@ -190,7 +190,7 @@ class RegistrationController extends BaseController
         if ($device_uid){
         $device  = $this->em->getRepository(Device::class)->findOneBy(array('device_uid' => $device_uid));
         if ($device instanceof Device){
-            $client_device = $device->getClient();
+            $client_device = $device->getClient()?:'';
             if ($client_device instanceof Client){
                 $client_device->setDevice(Null);
                 $this->em->flush();
