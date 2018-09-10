3664b48cac90b489cf4f90e6249e9cf957630ae4
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-10 11:39:16 +0100

    Notification columns

--- src/Entity/PreDeclaration.php
+++ src/Entity/PreDeclaration.php
@@ -163,7 +163,7 @@ class PreDeclaration
      * @Serializer\Expose()
      * @Serializer\Groups({"client_pre_declaration","listPreDeclaration"})
      *
-     * @ORM\ManyToOne(targetEntity="App\Entity\Client", inversedBy="preDeclaration",cascade={"persist", "remove"})
+     * @ORM\ManyToOne(targetEntity="App\Entity\Client", inversedBy="PreDeclaration",cascade={"persist", "remove"})
      */
     private $client;
 
