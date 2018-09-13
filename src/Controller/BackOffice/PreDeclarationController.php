fe48b64c4965419ac5a8db9cae11d67849afc694
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-13 09:04:20 +0100

    add notif when accept or reject predeclaration

--- src/Controller/BackOffice/PreDeclarationController.php
+++ src/Controller/BackOffice/PreDeclarationController.php
@@ -180,7 +180,7 @@ class PreDeclarationController extends Controller
 
         $resp = $pdtas->triggerPredeclaration($dataPre);
 
-        return $this->json(['Code' => $resp->code,'message' => $resp->code]);
+        //return $this->json(['Code' => $resp->code,'message' => $resp->code]);
 
 
         if ($resp->code == "200"){
