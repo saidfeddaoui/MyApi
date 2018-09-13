6f4bea10beda0f930d0cb1d494d596dc5ff79f7a
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-13 17:31:57 +0100

    images categories

--- src/Controller/Api/PreDeclarationController.php
+++ src/Controller/Api/PreDeclarationController.php
@@ -245,7 +245,8 @@ class PreDeclarationController extends BaseController
         $directory = $this->get('kernel')->getProjectDir() . '/public/img/tiers';
         $tiersAttachments = [];
         foreach ($request->files as $type) {
-
+             //en recois le nom de image xxx-12.png (exp)
+            //on prend juste la premier partie avant - pour la mettre comme type(categorie)
             $img_arr = explode("-", $type->getClientOriginalName());
             $types=$img_arr[0];
             /**
