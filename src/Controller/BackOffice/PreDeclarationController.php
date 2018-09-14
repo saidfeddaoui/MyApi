ba5ea163f94ff35484a9d0d1fa4e033da1fcaabb
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-14 12:57:21 +0100

    predeclaration stecture

--- src/Controller/BackOffice/PreDeclarationController.php
+++ src/Controller/BackOffice/PreDeclarationController.php
@@ -81,7 +81,7 @@ class PreDeclarationController extends Controller
     public function displayDetails(PreDeclaration $preDeclaration)
     {
         $attachements = $this->em->getRepository('App:TiersAttachment')->findByPreDeclaration($preDeclaration);
-        $sinistres = $this->em->getRepository('App:Item')->findBy(['id'=>$preDeclaration->getTypeSinistre(),'type' => 'sinistre']);
+        $sinistres = $this->em->getRepository('App:Item')->findBy(['id'=>$preDeclaration->getTypeSinistre(),'title' => 'sinistre']);
 
 
         return $this->render('pre_declaration/display_details.html.twig', [
