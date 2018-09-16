5e9e6e5a3a1cc27da6096649356d2ae87a7744eb
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-16 15:35:03 +0100

    fancybox

--- public/js/pre_declaration.js
+++ public/js/pre_declaration.js
@@ -1,4 +1,16 @@
 jQuery(document).ready(function() {
+
+    $(".fancybox")
+    .attr('rel', 'gallery')
+    .fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+        nextEffect  : 'none',
+        prevEffect  : 'none',
+        padding     : 0,
+        margin      : [20, 60, 20, 60] // Increase left/right margin
+    });
+    
     var ImageCrop = {
         init: function(photoPaneId, previewPaneId) {
             // Create variables (in this scope) to hold the API and image size
