f72998c6b944233137ebbfb3b0d43994a05d7ed9
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-18 11:33:34 +0100

    reste password

--- src/Entity/Contrats.php
+++ src/Entity/Contrats.php
@@ -15,6 +15,8 @@ class Contrats
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Serializer\Expose()
+     * @Serializer\Groups({"contrats","listPreDeclaration","client_pre_declaration"})
      *
      */
     private $id;
