11236eb47fec9221d919734014179cfb22e52f73
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-09-21 11:17:54 +0100

    remove btns predeclaration accept

--- public/js/devis_satisfaction.js
+++ public/js/devis_satisfaction.js
@@ -38,4 +38,13 @@ jQuery(document).ready(function() {
             [0, "desc"]
         ] // set first column as a default sort by asc
     });
+
+
+    $("body").on('click','.devis_satisfaction_details', function () {
+        var id = $(this).data('id');
+        url= Routing.generate('devis_satisfaction_details', {id: id});
+        window.open(url, '_blank');
+    });
+
+
 });
