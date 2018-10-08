9d3283ccd807fa4d44db22d640168b41325806b7
said <feddaoui@mobiblanc.com>
2018-10-08 11:55:34 +0100

    implementation mode Reparation back end + ws

---
+++ src/Controller/BackOffice/NewModeReparationController.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mobiblanc
+ * Date: 08/10/2018
+ * Time: 11:24
+ */
+
+namespace App\Controller\BackOffice;
+
+use App\Entity\ModeReparation;
+use App\Form\NewModeReparationType;
+use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class NewModeReparationController
+{
+
+    /**
+     * @Route(path="/mode_reparation",name="modes_reparation")
+     * @return Response
+     */
+    public function index()
+    {
+        $form = $this->createForm(NewModeReparationType::class, new ModeReparation(), [
+            'action' => $this->generateUrl('modes_reparation_add'),
+        ]);
+        $em = $this->getDoctrine()->getManager();
+        $mode = $em->getRepository('App:ModeReparation')->findAll();
+
+        return $this->render('modeReparation/index.html.twig', [
+            'page_title' => 'Mode Réparation',
+            'page_subtitle' => '',
+            'data' => $mode,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route(path="/add", name="add", options={"expose"=true})
+     *
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function add(Request $request, SessionInterface $session)
+    {
+        $form = $this->createForm(NewModeReparationType::class);
+        $form->handleRequest($request);
+        $em = $this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var Device $device
+             */
+            $mode = $form->getData();
+
+            $em->persist($mode);
+            $em->flush();
+        }
+        if ($errors = $form->getErrors()) {
+            foreach ($errors as $error) {
+                $this->addFlash('add.product.error', $error->getMessage());
+                break;
+            }
+        }
+        return  $this->redirect($this->generateUrl('modes_reparation'));
+    }
+
+    /**
+     * @Route(path="/edit/{id}", name="edit", options={"expose"=true})
+     *
+     * @param ModeReparation $mode
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(ModeReparation $mode, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(DeviceType::class, $mode,[
+            'action' => $this->generateUrl('modes_reparation_edit', ['id' => $mode->getId()])
+        ]);
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //$em->persist($mode);
+            $em->flush();
+            return $this->redirect($this->generateUrl('modes_reparation'));
+        }
+        return  $this->render('modeReparation/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route(path="/delete/{id}", name="delete", options={"expose"=true})
+     *
+     * @param ModeReparation $mode
+     * @return JsonResponse
+     */
+    public function delete(ModeReparation $mode)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($mode);
+        $em->flush();
+        return  new JsonResponse(['message' => 'Mode Réparation supprimée avec succès']);
+    }
+
+
+
+
+}
---
+++ src/Entity/ModeReparation.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mobiblanc
+ * Date: 08/10/2018
+ * Time: 11:03
+ */
+
+namespace App\Entity;
+
+
+class ModeReparation
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Serializer\Expose()
+     * @Serializer\Expose()
+     * @Serializer\Groups(groups={"modeReparation"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Serializer\Expose()
+     * @Serializer\Groups({"all"})
+     */
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+
+
+
+
+}
