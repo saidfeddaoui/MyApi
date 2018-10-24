4b0622fc20a96f1796846b3e61102535761d6921
fanane <fanane@mobiblanc.com>
2018-10-24 17:57:29 +0100

    Push avec les deux cle serveur a la base de CANAL

--- src/Services/ApiDevices.php
+++ src/Services/ApiDevices.php
@@ -28,7 +28,7 @@ class ApiDevices
     public function setDevice($device_params,$dateTime)
     {
         $devices = $this->em->getRepository(Device::class)->findOneBy(array('device_uid'=>$device_params['device_uid'],'canal'=>$device_params['canal']));
-        dump($devices->getCanal());die;
+        dump($devices);die;
         if (!$devices instanceof Device){
             $devices = new Device();
             $devices->setDeviceUid($device_params['device_uid']);
