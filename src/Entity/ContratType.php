616c0a114921fc488c47e71e5d782a6a36563f02
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-10-26 14:46:25 +0100

    add contrat type

--- src/Entity/ContratType.php
+++ src/Entity/ContratType.php
@@ -36,7 +36,7 @@ class ContratType
 
      /**
      * One Product has One Shipment.
-     * @OneToOne(targetEntity="InsuranceType")
+     * @OneToOne(targetEntity="App\Entity\InsuranceType")
      * @JoinColumn(name="insuranceType", referencedColumnName="id")
      */
     private $insuranceType;
