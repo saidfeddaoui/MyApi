7b80e824eb618ac7adfcd83cb7bde4310dbbb138
said <feddaoui@mobiblanc.com>
2018-10-29 17:17:12 +0000

    create web services List villes + list garages + envoi mode de réparation +

--- src/Controller/Api/DocController.php
+++ src/Controller/Api/DocController.php
@@ -13,7 +13,7 @@ use App\Services\DetailsContratApiService;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation\Model;
-use Symfony\Component\HttpFoundation\Response ;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 use Swagger\Annotations as SWG;
