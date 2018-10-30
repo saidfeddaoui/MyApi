e0c40df8a709c06af7f986d4f621d2927bc7dd96
said <feddaoui@mobiblanc.com>
2018-10-30 12:02:19 +0000

    create web services List villes + list garages + envoi mode de réparation +

--- src/Services/UploadDocApiService.php
+++ src/Services/UploadDocApiService.php
@@ -15,13 +15,43 @@ class UploadDocApiService extends ApiCustomerService
 
     public function UploadDocs($params)
     {
-        $requestDoc = json_encode($params);
+        /*£$requestDoc = json_encode($params);
         $response = $this->httpClient->post("", [
             'body' => $requestDoc,
             'headers' => ['Content-type' => 'application/json']
         ]);
         //var_dump($response->getBody());die;
-        return $this->getResult($response);
+        return $this->getResult($response);*/
+
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://ais.mamda-mcma.ma/WS/api/mode/reparation/getdoc",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($params),
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "content-type: application/json",
+                "postman-token: 6fa3d47e-df1f-b794-fac8-b5e40d2f99ca"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return null;
+        } else {
+            return $response;
+        }
     }
 
     protected function getResult($response)
