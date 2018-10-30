f07e31624e00e11e38dfef26908c61ace890423b
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-10-30 14:18:45 +0000

    upper case name

--- src/Controller/Api/RegistrationController.php
+++ src/Controller/Api/RegistrationController.php
@@ -346,9 +346,8 @@ class RegistrationController extends BaseController
 
 
         $nom = strtoupper($submittedClient->getFamilyName());
-        // $prenom = ucfirst($submittedClient->getFirstName());
-         $fname = $submittedClient->getFirstName();
-         $prenom = strtoupper(substr($fname, 0,1)).substr($fname,1); 
+        $prenom = ucfirst(strtolower($submittedClient->getFirstName()));
+
 
 
         $client
