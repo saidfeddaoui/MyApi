e5d7ff84ec404dcbb06cf83695711f1ed747d95d
said <feddaoui@mobiblanc.com>
2018-11-01 08:45:53 +0000

    add ajout mode reparation to predeclaration

--- src/Controller/BackOffice/PreDeclarationController.php
+++ src/Controller/BackOffice/PreDeclarationController.php
@@ -267,14 +267,15 @@ class PreDeclarationController extends Controller
 
         $choixMDR = $this->em->getRepository('App:ChoixMDR')->findByPreDeclaration($preDeclaration);
 
-        $i=0;
+
         foreach ($choixMDR as $objetMDR){
 
-            //if(!in_array($objetMDR->getId(),array_values($valeursMDR))){
+            if(!in_array($objetMDR->getId(),array_values($valeursMDR))){
                 $objetMDR->setStatut(false);
-            //}
-            $i++;
+            }
+
         }
+        $this->em->flush();
 
 
         $dataPre=json_decode(json_encode($preDeclarationInfo),true);;
