1ad94ba87029c940b9f06d064299721c7b44b7f1
fanane <fanane@mobiblanc.com>
2018-11-27 10:14:26 +0000

    Ajout du champ "cible" dans le contrôle de version

--- src/Services/SmsApiService.php
+++ src/Services/SmsApiService.php
@@ -20,17 +20,10 @@ class SmsApiService extends ApiCustomerService
                 'app_id' => self::APP_ID,
                 'senderid' => self::SENDER_ID,
                 'msg' => $message,
-                'tel' => $this->standardizeTel($phoneNumber)
+                'tel' => $phoneNumber
             ]
         ]);
     }
 
-    public function standardizeTel($phoneNumber) {
-        if(strpos($phoneNumber, '0') === 0) {
-            $phoneNumber = '212' . substr(str_replace(' ', '', $phoneNumber), 1);
-        }
-
-        return $phoneNumber;
-    }
 
 }
