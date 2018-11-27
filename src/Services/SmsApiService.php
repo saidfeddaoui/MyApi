187fdd7d8cdbd6608790dfeb33febbfa54fd3c01
fanane <fanane@mobiblanc.com>
2018-11-27 10:37:52 +0000

    Ajout du champ "cible" dans le contrôle de version

--- src/Services/SmsApiService.php
+++ src/Services/SmsApiService.php
@@ -14,21 +14,15 @@ class SmsApiService extends ApiCustomerService
      */
     public function sendSms(string $phoneNumber, string $message)
     {
-        $Action = 'SMS';
-        $AppId = self::APP_ID;
-        $SenderId = self::SENDER_ID;
-        $message = 'TEST '.$message;
-        $Tele = $phoneNumber;
-        $url_sms = "http://smsprod.mobiblanc.com/api.php/send_sms/send";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url_sms);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "action=" . $Action . "&app_id=" . $AppId . "&senderid=" . $SenderId . "&msg=" . $message . "" . "&tel=" . $Tele . "");
-        $result = curl_exec($ch);
-        curl_close($ch);
-        var_dump($result);die;
-
+        $this->httpClient->get('send', [
+            'query' => [
+                'action' => 'SMS',
+                'app_id' => self::APP_ID,
+                'senderid' => self::SENDER_ID,
+                'msg' => $message,
+                'tel' => $phoneNumber
+            ]
+        ]);
     }
 
 
