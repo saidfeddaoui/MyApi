ace7616f97d40141cb6f64ed7baca8eee1d66300
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-11-30 12:54:13 +0100

    devis

--- public/js/mrh.js
+++ public/js/mrh.js
@@ -22,6 +22,10 @@ jQuery(document).ready(function() {
                 "orderable": true,
             },
             { // set default column settings
+                "searchable": true,
+                "orderable": true,
+            },
+            { // set default column settings
                 "searchable": false,
                 "orderable": false,
             }
