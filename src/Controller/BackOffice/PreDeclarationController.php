7ca6a883a976e21dbdb18c19ff5107d6f36675ab
Ahargoug yassine <ahargoug@mobiblanc.com>
2018-12-06 16:17:19 +0100

    remarque mamda

--- src/Controller/BackOffice/PreDeclarationController.php
+++ src/Controller/BackOffice/PreDeclarationController.php
@@ -21,6 +21,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Services\PreDeclarationTriggerApiService;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * @Route(path="/pre_declarations", name="pre_declarations_")
